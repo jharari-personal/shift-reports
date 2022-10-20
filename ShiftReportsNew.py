1aa29fd1ff8c71d3b3913e1cab11e7874f7b29b4
jharari-personal <82702084+jharari-personal@users.noreply.github.com>
2022-10-20 20:31:13 +0300

    minor cleanup, added comments

--- ShiftReportsNew.py
+++ ShiftReportsNew.py
@@ -1,25 +1,12 @@
 # -*- coding: utf-8 -*-
 
-#last change - August 16 2022
+#last change - October 20th 2022
 
 import tkinter as tk
 from datetime import datetime
 from os import getenv, startfile
 
 # assigning the basic variables
-
-##repsAPAC = "X:\\Sales\\Shortcuts\\!Shift_Reports_Folder\\repsAPAC.txt
-##repsAU = "X:\\Sales\\Shortcuts\\!Shift_Reports_Folder\\repsAU.txt
-##repsChina = "X:\\Sales\\Shortcuts\\!Shift_Reports_Folder\\repsChina.txt
-##repsEU = "X:\\Sales\\Shortcuts\\!Shift_Reports_Folder\\repsEU.txt
-##repsFSR = "X:\\Sales\\Shortcuts\\!Shift_Reports_Folder\\repsFSR.txt"
-##ShiftReportsAPAC = "X:\\Sales\\Shortcuts\\!Shift_Reports_Folder\\ShiftReportsAPAC.txt
-##ShiftReportsAU = "X:\\Sales\\Shortcuts\\!Shift_Reports_Folder\\ShiftReportsAU.txt
-##ShiftReportsChina = "X:\\Sales\\Shortcuts\\!Shift_Reports_Folder\\ShiftReportsChina.txt
-##ShiftReportsEU = "X:\\Sales\\Shortcuts\\!Shift_Reports_Folder\\ShiftReportsEU.txt
-##ShiftReportsFSR = "X:\\Sales\\Shortcuts\\!Shift_Reports_Folder\\ShiftReportsFSR.txt
-
-
 Login = getenv('username').lower()
 lines = "---"*10
 reps = []
@@ -37,7 +24,6 @@ euButton = tk.Button(master, padx=0, pady=0, image=pixel, width=70, height=70, c
 apacButton = tk.Button(master, padx=0, pady=0, image=pixel, width=70, height=70, compound="c",text = "APAC", bg="lightyellow",  command = lambda: select_team("APAC"))
 auButton = tk.Button(master, padx=0, pady=0, image=pixel, width=70, height=70, compound="c",text = "AU", bg="lightyellow",  command = lambda: select_team("AU"))
 quitButton = tk.Button(master, padx=0, pady=0, image=pixel, width=70, height=70, compound="c", text="Quit", bg="pink", command=master.quit)
-
 
 #input boxes setup
 foo = tk.StringVar()
@@ -63,13 +49,13 @@ def clear_buttons():
 	for s in slaves:
 		s.grid_forget()
 
-#this function clears the text
+#this function clears the text in the labels
 def clear_text():
 	foo.set("")
 	foo2.set("")
 	foo3.set("")
 
-#laying first content
+#laying first content (the team selector)
 def first_screen():
 	clear_buttons()
 	
@@ -115,6 +101,7 @@ def recreate_buttons():
 	optionsButton.grid(row=3,column=3)
 	#clear_text()
 
+#this function sets up all the buttons and labels for the main shift report layout
 def laying_content(team):
 	clear_buttons()
 	lblInst = tk.Label(master, text="Using %s team's file" % team, background='#f2f2f2', font=("Helvetica", 12))
@@ -135,22 +122,22 @@ def laying_content(team):
 
 #this function is used when user clicks on "Send"
 def Write():  
-	NiceNow = getDate()
+	NiceNow = getDate() #gets the date
 	lblInst = tk.Label(master, text="Shift Report Logged, on %s." % NiceNow, background='#f2f2f2', font=("Helvetica", 12))
 	typedSR = srEntry.get()
-	typedCountry = countryEntry.get()
-	typedAccount = accountEntry.get()
-	if not typedSR:
-		laying_content(team)
+	typedCountry = countryEntry.get() #assigns whatever the user wrote to typedCountry
+	typedAccount = accountEntry.get() #assigns whatever the user wrote to typedAccount
+	if not typedSR: #if empty
+		laying_content(team) #recreate GUI
 		label_noSR.grid(row=5, column=3)
 		return
-	if not typedCountry and not typedAccount:
-		laying_content(team)
+	if not typedCountry and not typedAccount: #if both are empty
+		laying_content(team) #recreate GUI
 		label_noCountry.grid(row=5, column=3)
 		return
 		
 	try:
-				with open(srLocation, 'a', encoding="utf8",  errors='ignore') as file:
+				with open(srLocation, 'a', encoding="utf8",  errors='ignore') as file: #note that srLocation is the one we got in select_team()
 						file.write("New Shift Report \nLogged by: " + str(Login) + "\nDate Logged: " + str(NiceNow) + "\nCountry: " + typedCountry.capitalize() + "\nAccount Number: " + typedAccount + "\nReport: " + typedSR + "\n" + lines + "\n")
 	except FileNotFoundError:
 		laying_content(team)
@@ -161,18 +148,18 @@ def Write():
 
 	lblInst.grid(row=5, column=3)
 	
-
+#this function counts the number of reports logged by the user running the file
 def Count():
-	clear_buttons()
-	more_options()
+	clear_buttons() 
+	more_options() #just recreates the GUI
 	count = 0
 	try:
 		with open(srLocation, 'r', encoding="utf8",  errors='ignore') as f:
-			for line in f.readlines():
-				words = line.lower().split()
-				for word in words:
-					if word == Login:
-						count += 1
+			for line in f.readlines(): #takes every single line in the file
+				words = line.lower().split() #takes every single word in the file
+				for word in words: 
+					if word == Login: #checks if each word is the same as the user's login
+						count += 1 #if so, add one to "count"
 		lblInst = tk.Label(master, text="Number of reports you have submitted this quarter: %s" % count, background='#f2f2f2', font=("Helvetica", 12))
 		lblInst.grid(row=8, column=2)
 	except FileNotFoundError:
@@ -182,7 +169,7 @@ def Count():
 		
 			
 
-
+#this function opens the text file
 def OpenFile():
 	clear_buttons()
 	more_options()
@@ -200,7 +187,7 @@ def OpenFile():
 			
 
 
-
+#this function creates the sub-GUI when user clicks on More Options
 def more_options():
 	clear_buttons()
 	homeButton.grid(row=1,column=2)
@@ -210,7 +197,7 @@ def more_options():
 	quitButton.grid(row=5,column=2)
 
 
-
+#this function exports the shift reports into a CSV
 def export():
 	rep_counts = {rep: 0 for rep in reps}
 	label_nop = tk.Label(master, text="The widget does not have permissions to the CSV file.\nPlease make sure you close the Results.CSV file before trying to export the results again.", background='#f2f2f2', font=("Helvetica", 11))
@@ -252,7 +239,6 @@ def export():
 
 
 #secondary buttons setup
-
 sendButton = tk.Button(master, text = "Send", bg="lightyellow", command = Write)
 openButton = tk.Button(master, text = "Open Shift Reports File", bg="lightyellow", command = OpenFile)
 countButton = tk.Button(master, text = "Count Your Reports", bg="lightyellow", command = Count)
@@ -273,35 +259,4 @@ def main_function():
 
 first_screen()
 tk.mainloop()
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-	
-
-	
-
-
-
-
-
-
-
-
 
